728e0f49945b25f9ddc376a74194598f07e571d6
AhmadAlharbi <ahmaadzaid7@gmail.com>
2023-05-22 10:07:55 +0300

    CRUD Exams Table

--- app/Providers/RepoServiceProvider.php
+++ app/Providers/RepoServiceProvider.php
@@ -57,6 +57,10 @@ class RepoServiceProvider extends ServiceProvider
             'App\Repository\SubjectRepositoryInterface',
             'App\Repository\SubjectRepository'
         );
+        $this->app->bind(
+            'App\Repository\ExamRepositoryInterface',
+            'App\Repository\ExamRepository'
+        );
     }
 
     /**
